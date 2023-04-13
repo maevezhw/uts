b9f4ec5cc610e466cd344d83599cb4199f40b515
maevezhw <112373358+maevezhw@users.noreply.github.com>
2023-04-13 12:15:34 +0700

    admin view

---
+++ admin.js
@@ -0,0 +1,248 @@
+import React from 'react'
+
+import './admin.css'
+
+const Admin = (props) => {
+  return (
+    <div>
+      <div className="admin-admin-view-customers-products-orders-payments-shipping-my">
+        <div className="admin-frame70">
+          <img
+            src="/playground_assets/rectangle483503-6cgr-2100h.png"
+            alt="Rectangle483503"
+            className="admin-rectangle48"
+          />
+        </div>
+        <div className="admin-frame71">
+          <img
+            src="/playground_assets/rectangle493503-bgb-400w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle49"
+          />
+          <span className="admin-text">
+            <span>DASHBOARD</span>
+          </span>
+          <img
+            src="/playground_assets/vector3503-lemi.svg"
+            alt="Vector3503"
+            className="admin-vector"
+          />
+          <div className="admin-frame81">
+            <span className="admin-text02">
+              <span>CUSTOMERS</span>
+            </span>
+            <div className="admin-frame86">
+              <span className="admin-text04">
+                <span>Search</span>
+              </span>
+            </div>
+            <div className="admin-frame87">
+              <span className="admin-text06">
+                <span>Filter</span>
+              </span>
+            </div>
+            <img
+              src="/playground_assets/eosfilterlistfilled3503-ht7i.svg"
+              alt="EOSFILTERLISTFILLED3503"
+              className="admin-eosfilterlistfilled"
+            />
+            <div className="admin-frame89">
+              <img
+                src="/playground_assets/vector3503-21m.svg"
+                alt="Vector3503"
+                className="admin-vector01"
+              />
+              <span className="admin-text08">
+                <span>Add new customer</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="admin-frame72">
+          <img
+            src="/playground_assets/rectangle493503-v2m5-300w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle491"
+          />
+          <span className="admin-text10">
+            <span>MESSAGES</span>
+          </span>
+          <img
+            src="/playground_assets/vector3503-1qh.svg"
+            alt="Vector3503"
+            className="admin-vector02"
+          />
+          <img
+            src="/playground_assets/line103503-enre.svg"
+            alt="Line103503"
+            className="admin-line10"
+          />
+          <img
+            src="/playground_assets/rectangle503503-njw5-200h.png"
+            alt="Rectangle503503"
+            className="admin-rectangle50"
+          />
+        </div>
+        <div className="admin-frame73">
+          <img
+            src="/playground_assets/rectangle493503-77hn-300w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle492"
+          />
+          <span className="admin-text12">
+            <span>CUSTOMERS</span>
+          </span>
+          <img
+            src="/playground_assets/vector3503-t6hg.svg"
+            alt="Vector3503"
+            className="admin-vector03"
+          />
+        </div>
+        <div className="admin-frame74">
+          <img
+            src="/playground_assets/rectangle493503-ggan-400w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle493"
+          />
+          <span className="admin-text14">
+            <span>PRODUCTS</span>
+          </span>
+          <img
+            src="/playground_assets/vector3503-p9eh.svg"
+            alt="Vector3503"
+            className="admin-vector04"
+          />
+        </div>
+        <div className="admin-frame75">
+          <img
+            src="/playground_assets/rectangle493503-hzau-400w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle494"
+          />
+          <span className="admin-text16">
+            <span>ORDERS</span>
+          </span>
+          <img
+            src="/playground_assets/vector3503-lc5u.svg"
+            alt="Vector3503"
+            className="admin-vector05"
+          />
+        </div>
+        <div className="admin-frame77">
+          <img
+            src="/playground_assets/rectangle493503-tqi9-400w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle495"
+          />
+          <span className="admin-text18">
+            <span>PAYMENTS</span>
+          </span>
+          <img
+            src="/playground_assets/vector3503-u00d.svg"
+            alt="Vector3503"
+            className="admin-vector06"
+          />
+        </div>
+        <div className="admin-frame78">
+          <img
+            src="/playground_assets/rectangle493503-fwfh-400w.png"
+            alt="Rectangle493503"
+            className="admin-rectangle496"
+          />
+          <span className="admin-text20">
+            <span>REVIEWS</span>
+          </span>
+          <img
+            src="/playground_assets/vector3504-06hd.svg"
+            alt="Vector3504"
+            className="admin-vector07"
+          />
+        </div>
+        <div className="admin-frame79">
+          <img
+            src="/playground_assets/rectangle493504-mvpx-400w.png"
+            alt="Rectangle493504"
+            className="admin-rectangle497"
+          />
+          <span className="admin-text22">
+            <span>MY STAFF</span>
+          </span>
+          <img
+            src="/playground_assets/vector3504-o82o.svg"
+            alt="Vector3504"
+            className="admin-vector08"
+          />
+        </div>
+        <div className="admin-frame80">
+          <img
+            src="/playground_assets/rectangle493504-slk-400w.png"
+            alt="Rectangle493504"
+            className="admin-rectangle498"
+          />
+          <span className="admin-text24">
+            <span>SETTINGS</span>
+          </span>
+          <img
+            src="/playground_assets/vector3504-jt2o.svg"
+            alt="Vector3504"
+            className="admin-vector09"
+          />
+        </div>
+        <div className="admin-frame17">
+          <img
+            src="/playground_assets/vector3504-snnd.svg"
+            alt="Vector3504"
+            className="admin-vector10"
+          />
+          <img
+            src="/playground_assets/eosarrowdropdownfilled3504-aj5.svg"
+            alt="EOSARROWDROPDOWNFILLED3504"
+            className="admin-eosarrowdropdownfilled"
+          />
+          <div className="admin-frame16">
+            <div className="admin-frame-kiddos"></div>
+            <img
+              src="/playground_assets/eosmarkemailunreadfilled3504-eqrt.svg"
+              alt="EOSMARKEMAILUNREADFILLED3504"
+              className="admin-eosmarkemailunreadfilled"
+            />
+            <span className="admin-text26">|</span>
+            <span className="admin-text27">
+              <span>Admin</span>
+            </span>
+            <span className="admin-text29">
+              <span>Brandify</span>
+            </span>
+          </div>
+        </div>
+        <img
+          src="/playground_assets/line93504-4sms.svg"
+          alt="Line93504"
+          className="admin-line9"
+        />
+        <img
+          src="/playground_assets/image243504-gis-1000w.png"
+          alt="image243504"
+          className="admin-image24"
+        />
+        <img
+          src="/playground_assets/eosmodeeditfilled3504-391e.svg"
+          alt="EOSMODEEDITFILLED3504"
+          className="admin-eosmodeeditfilled"
+        />
+        <img
+          src="/playground_assets/eosdeletefilled3504-mkc7.svg"
+          alt="EOSDELETEFILLED3504"
+          className="admin-eosdeletefilled"
+        />
+        <img
+          src="/playground_assets/eosmorehorizfilled3504-2bdk.svg"
+          alt="EOSMOREHORIZFILLED3504"
+          className="admin-eosmorehorizfilled"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Admin
